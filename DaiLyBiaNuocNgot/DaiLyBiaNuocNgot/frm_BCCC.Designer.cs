dd9e16ae78fcd55a0a8a45fb8929c86dc26f790f
hung <tqhungdev@gmail.com>
2018-11-01 19:19:42 +0700

    first commit

---
+++ DaiLyBiaNuocNgot/DaiLyBiaNuocNgot/frm_BCCC.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DaiLyBiaNuocNgot
+{
+    partial class frm_BCCC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.d2 = new System.Windows.Forms.DateTimePicker();
+            this.d1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(976, 168);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 39);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Đến";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(621, 168);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 39);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Từ";
+            // 
+            // d2
+            // 
+            this.d2.CustomFormat = "yyyy/MM/dd";
+            this.d2.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.d2.Location = new System.Drawing.Point(1061, 160);
+            this.d2.Margin = new System.Windows.Forms.Padding(6);
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(252, 47);
+            this.d2.TabIndex = 33;
+            // 
+            // d1
+            // 
+            this.d1.CustomFormat = "yyyy/MM/dd";
+            this.d1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.d1.Location = new System.Drawing.Point(689, 160);
+            this.d1.Margin = new System.Windows.Forms.Padding(6);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(252, 47);
+            this.d1.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(726, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(839, 73);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "BÁO CÁO NHÀ CUNG CẤP";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1421, 125);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 83);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Xuất báo cáo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = -1;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(13, 225);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.Size = new System.Drawing.Size(2224, 1355);
+            this.crystalReportViewer2.TabIndex = 37;
+            // 
+            // frm_BCCC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(226)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(2236, 1580);
+            this.Controls.Add(this.crystalReportViewer2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.d2);
+            this.Controls.Add(this.d1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frm_BCCC";
+            this.Text = "frm_BCCC";
+            this.Load += new System.EventHandler(this.frm_BCCC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker d2;
+        private System.Windows.Forms.DateTimePicker d1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+    }
+}
